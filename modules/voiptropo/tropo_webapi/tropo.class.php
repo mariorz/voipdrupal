81382bc232028a69f3ed9a38ea6fe9ff90180d6a
Tamer Zoubi <tamerzg@gmail.com>
2012-06-14 17:34:43 +0200

    reverted change in tropo.class.php field_file_save_file replaced with _voipplivo_file_save_upload() removed commented code

--- modules/voiptropo/tropo_webapi/tropo.class.php
+++ modules/voiptropo/tropo_webapi/tropo.class.php
@@ -303,10 +303,7 @@ class Tropo extends BaseClass {
 	  	$voice = isset($voice) ? $voice : $this->_voice;
 	  	$say = new Say($value, $as, $event, $voice, $allowSignals);
 		}
-        //@tamerzg: remove sprintf it was throwing error when urls where encoded.
-		$this->say = array(sprintf($say));
-//watchdog('voiptropo','say '.print_r($say,true). 'say sprintf'.print_r(sprintf($say),true));        
-        //$this->say = array($say);	
+		$this->say = array(sprintf($say));      
 	}
 	
 	/**
